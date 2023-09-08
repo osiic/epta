103703371198331ed4cb50cc5d9a57cbbc81b18a
Osi ic <osiic.offcl@gmail.com>
2023-09-08 08:19:48 +0700

    feat : udah ada memori

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
-require('dotenv').config();
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { createChatCompletion } = require('./openai');
+require("dotenv").config();
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { createChatCompletion } = require("./openai");
 
 const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildMembers,
-	],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
 });
 const token = process.env.DISCORD_TOKEN;
 
 client.once(Events.ClientReady, (c) => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+  console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
 client.on(Events.MessageCreate, async (msg) => {
-	if (msg.author.bot || msg.channelId != '1058229261552599060') return;
+  if (msg.author.bot || msg.channelId != "1058229261552599060") return;
 
-	if (msg.content) {
-		const chat = await createChatCompletion(msg.content, msg.author.username);
-		if (chat) {
-			await msg.channel.send(chat);
-		}
-	}
+  if (msg.content) {
+    msg.channel.startTyping();
+
+    const chat = await createChatCompletion(msg.content, msg.author.username);
+    if (chat) {
+      await msg.channel.send(chat);
+    }
+  }
 });
 
 client.login(token);
--- src/openai/index.js
+++ src/openai/index.js
@@ -1,26 +1,36 @@
-const OpenAI = require('openai');
+const OpenAI = require("openai");
 const openai = new OpenAI({
-	apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
-const pesan = require('./pesan.json');
-const { tambahPesan } = require('./tambahPesan.js');
+const pesan = require("./pesan.json");
+const { tambahPesan } = require("./tambahPesan.js");
 
-const createChatCompletion = async (prompt, user = 'Penggemar') => {
-	pesan.push({ role: 'user', content: `${user} berkata ${prompt}` });
+const createChatCompletion = async (prompt, user = "Penggemar") => {
+  const sanitizedUser = user.replace(/[^a-zA-Z0-9_-]/g, ""); // Remove characters that don't match the pattern
 
-	const completion = await openai.chat.completions.create({
-		model: 'gpt-3.5-turbo',
-		messages: pesan,
-		max_tokens: 256,
-		temperature: 1,
-		top_p: 0.9,
-	});
+  pesan.push({
+    role: "user",
+    name: sanitizedUser,
+    content: prompt,
+  });
 
-	const generatedMessage = completion.choices[0].message.content;
-	await pesan.push({ role: 'assistant', content: generatedMessage });
-	await tambahPesan(pesan);
-	return generatedMessage;
+  const completion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: pesan,
+    max_tokens: 256,
+    temperature: 1,
+    top_p: 0.9,
+  });
+
+  const generatedMessage = completion.choices[0].message.content;
+  await pesan.push({
+    role: "assistant",
+    name: "epta",
+    content: generatedMessage,
+  });
+  await tambahPesan(pesan);
+  return generatedMessage;
 };
 
 module.exports = { createChatCompletion };
